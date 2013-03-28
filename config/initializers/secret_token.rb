c3a55fde4d5776a7d8acf49d17a7ee7cb45381e6
Wei <jrweizhang@gmail.com>
2013-03-28 11:38:05 +0800

    init commit site.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,8 @@
+# -*- encoding : utf-8 -*-
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bambooweek::Application.config.secret_token = 'f43a22d3c4ba53209f050542db571c6e1dbf9923060ac7201c9f77800da823800d9470fe32d103d07ac2955fd1a1ee359631da2486118c3b5d03cb8f060150ee'
