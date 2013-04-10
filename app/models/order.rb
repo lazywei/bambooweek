8d06bcc3d734b01e13724c98853cf02a33f69176
Wei <jrweizhang@gmail.com>
2013-04-10 23:00:06 +0800

    Modified order create process.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,12 @@
 class Order < ActiveRecord::Base
   has_many :order_itemships, :dependent => :destroy
   has_many :items, :through => :order_itemships
-  attr_accessible :email, :mobile, :name, :stid, :item_ids, :draw_date
+  attr_accessible :email, :mobile, :name, :stid, :item_ids, :draw_date, :note
 
-  validates_presence_of :name, :email, :mobile
+  validates_presence_of :name, :email, :mobile, :draw_date
   validates_format_of :email, :with => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   validates_format_of :mobile, :with => /^\d*$/
-  validates :draw_date, :inclusion => {:in => [509, 510], :message => "%{value} is not a valid draw date"}
+  validates :draw_date, :inclusion => {:in => [515, 516], :message => "%{value} is not a valid draw date"}
 
   def total_price
     total = 0
